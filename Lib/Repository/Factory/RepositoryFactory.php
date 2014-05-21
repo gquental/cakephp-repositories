6adde7c5aea0a4650ca16432156bb8da285b0672
Peter Moraes <peter.veiga.moraes@gmail.com>
2014-05-20 22:51:28 -0300

    Removed the static method so you can instantiate the repository with the method getRepository

--- Lib/Repository/Factory/RepositoryFactory.php
+++ Lib/Repository/Factory/RepositoryFactory.php
@@ -3,7 +3,7 @@ App::uses('RepositoryException', 'Repositories.Lib/Repository/Exception');
 
 class RepositoryFactory {
 
-	public static function getRepository($name) {
+	public function getRepository($name) {
 		$repositoryName = $name . 'Repository';
 		$repositoryPath = ROOT . DS . APP_DIR . DS . 'Repository' . DS . $repositoryName . '.php';
 
